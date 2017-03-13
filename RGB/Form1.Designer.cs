0726a61c127f09ba397f3ce7f807f2e5fe757913
Javonmeng <javonmeng@gmail.com>
2017-03-13 16:51:22 +0800

    demo

---
+++ RGB/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace RGB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.R_Text = new System.Windows.Forms.TextBox();
+            this.G_Text = new System.Windows.Forms.TextBox();
+            this.B_Text = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.X_Text = new System.Windows.Forms.TextBox();
+            this.Y_Text = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.RGBBOX = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBBOX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // R_Text
+            // 
+            this.R_Text.Location = new System.Drawing.Point(739, 69);
+            this.R_Text.Name = "R_Text";
+            this.R_Text.Size = new System.Drawing.Size(92, 21);
+            this.R_Text.TabIndex = 0;
+            // 
+            // G_Text
+            // 
+            this.G_Text.Location = new System.Drawing.Point(739, 96);
+            this.G_Text.Name = "G_Text";
+            this.G_Text.Size = new System.Drawing.Size(92, 21);
+            this.G_Text.TabIndex = 1;
+            // 
+            // B_Text
+            // 
+            this.B_Text.Location = new System.Drawing.Point(739, 123);
+            this.B_Text.Name = "B_Text";
+            this.B_Text.Size = new System.Drawing.Size(92, 21);
+            this.B_Text.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(739, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "打开图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(525, 512);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // X_Text
+            // 
+            this.X_Text.Location = new System.Drawing.Point(739, 15);
+            this.X_Text.Name = "X_Text";
+            this.X_Text.Size = new System.Drawing.Size(92, 21);
+            this.X_Text.TabIndex = 5;
+            this.X_Text.Tag = "";
+            // 
+            // Y_Text
+            // 
+            this.Y_Text.Location = new System.Drawing.Point(739, 42);
+            this.Y_Text.Name = "Y_Text";
+            this.Y_Text.Size = new System.Drawing.Size(92, 21);
+            this.Y_Text.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(739, 319);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "获取RGB";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(716, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(715, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Y:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(716, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "R:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(716, 99);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "G:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(716, 126);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "B:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Tekton Pro Ext", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(543, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 197);
+            this.textBox1.TabIndex = 14;
+            // 
+            // RGBBOX
+            // 
+            this.RGBBOX.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.RGBBOX.Location = new System.Drawing.Point(739, 162);
+            this.RGBBOX.Name = "RGBBOX";
+            this.RGBBOX.Size = new System.Drawing.Size(92, 50);
+            this.RGBBOX.TabIndex = 15;
+            this.RGBBOX.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(739, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 29);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "RGB生成";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(869, 561);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.RGBBOX);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Y_Text);
+            this.Controls.Add(this.X_Text);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.B_Text);
+            this.Controls.Add(this.G_Text);
+            this.Controls.Add(this.R_Text);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RGB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RGBBOX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox R_Text;
+        private System.Windows.Forms.TextBox G_Text;
+        private System.Windows.Forms.TextBox B_Text;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox X_Text;
+        private System.Windows.Forms.TextBox Y_Text;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox RGBBOX;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ RGB/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace RGB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Size = new Size(512,512);
+            openFileDialog1.ShowDialog();
+            Image my_Image = System.Drawing.Image.FromFile(openFileDialog1.FileName);
+            pictureBox1.Image = my_Image;
+            pictureBox1.Size = GetPictureDisplaySize(pictureBox1).Size;
+            string my_Image_Info, Name, Size;
+            Name = "Name:" + openFileDialog1.FileName + "\r\n\r\n";
+            Size = "Width:" + my_Image.Width + "\r\n" + "Height:" + my_Image.Height;
+            my_Image_Info = Name + Size;
+            textBox1.Text = my_Image_Info;
+        }
+
+        public Rectangle GetPictureDisplaySize(PictureBox pbxImage)
+        {
+            if (pbxImage != null)
+            {
+                PropertyInfo ppiImageRect = pbxImage.GetType().GetProperty("ImageRectangle", BindingFlags.Instance | BindingFlags.NonPublic);
+                return (Rectangle)ppiImageRect.GetValue(pbxImage, null);
+            }
+            return new Rectangle(0, 0, 1, 1);
+        }
+
+
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            try
+            {
+                Color pointColor = bmp.GetPixel(e.X, e.Y);
+                X_Text.Text = e.X.ToString();
+                Y_Text.Text = e.Y.ToString();
+                R_Text.Text = pointColor.R.ToString();
+                G_Text.Text = pointColor.G.ToString();
+                B_Text.Text = pointColor.B.ToString();
+                RGBBOX.BackColor = pointColor;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = (Bitmap)pictureBox1.Image;
+            try
+            {
+                int X = Convert.ToInt32(X_Text.Text);
+                int Y= Convert.ToInt32(Y_Text.Text);
+                Color pointColor = bmp.GetPixel(X, Y);
+
+                R_Text.Text = pointColor.R.ToString();
+                G_Text.Text = pointColor.G.ToString();
+                B_Text.Text = pointColor.B.ToString();
+                RGBBOX.BackColor = pointColor;
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+           
+                RGBBOX.BackColor = Color.FromArgb(Convert.ToInt32(R_Text.Text), Convert.ToInt32(G_Text.Text), Convert.ToInt32(B_Text.Text)); ;
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
